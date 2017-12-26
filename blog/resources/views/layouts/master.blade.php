3fe5204a35c6f2a7b2e293d9356959b3a47c2214
Juan David Sánchez <juancho85@gmail.com>
2017-12-26 21:41:55 +0100

    add basic styling

--- blog/resources/views/layouts/master.blade.php
+++ blog/resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ URL::secure('css/app.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
     @yield('styles')
 </head>
 <body>
